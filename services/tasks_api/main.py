66064a99e08c44d3f4411526b4a47b91c12b198c
JoachimVanderbeke_CloudsOfCare <joachim.vanderbeke@cloudsofcare.com>
2025-06-16 08:54:12 +0200

    add deploy config debug

--- services/tasks_api/main.py
+++ services/tasks_api/main.py
@@ -1,3 +1,7 @@
+# Want to test it out? Run
+# poetry run uvicorn main:app --reload
+# Then, navigate to http://127.0.0.1:8000/api/health-check/ in your browser. You should see {"message": "OK"}.
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from mangum import Mangum  # new
