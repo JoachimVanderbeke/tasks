446a1fed1ce581b92ab6043eba5c37500a52c2c7
JoachimVanderbeke_CloudsOfCare <joachim.vanderbeke@cloudsofcare.com>
2025-06-18 10:09:42 +0200

    add cognito to app

--- services/tasks_ui/vite.config.js
+++ services/tasks_ui/vite.config.js
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // prevent a build error when using AWS Amplify libraries with Vite
+  // https://github.com/aws/aws-sdk-js/issues/3673
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
+    },
+  }
+});
