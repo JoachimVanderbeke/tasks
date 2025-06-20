a945a6931dc1c979b3b6d2102576833bcbe19728
JoachimVanderbeke_CloudsOfCare <joachim.vanderbeke@cloudsofcare.com>
2025-06-20 12:14:26 +0200

    update user pool id and userPoolWebClientId

--- services/tasks_ui/src/aws-exports.js
+++ services/tasks_ui/src/aws-exports.js
@@ -1,7 +1,7 @@
 export default {
   Auth: {
     region: 'eu-west-1',
-    userPoolId: 'eu-west-1_0uu9qs8UP',
-    userPoolWebClientId: '32v37t0totkqjovgh55ih08ei4'
+    userPoolId: 'eu-west-1_ORPlOiCCs',
+    userPoolWebClientId: '7vejkbl6qlrha3vlu8mfohl57e'
   }
 };
